8c7651bff6391c0e589cc3a4eff4c36c4cc849fa
Avagenzo <johann.martens@smail.inf.h-brs.de>
2020-04-14 21:04:49 +0200

    white icon

--- sfnewsticker/src/sfnewsticker/app.py
+++ sfnewsticker/src/sfnewsticker/app.py
@@ -10,7 +10,8 @@ from toga.style.pack import Pack, COLUMN
 
 def build(app):
     main_box = toga.Box(style=Pack(direction=COLUMN, padding_top=50))
-    settings_icon = "icons/baseline_settings_black_18dp.png"
+    settings_icon = "icons/baseline_settings_white_48dp.png"
+    search_icon = "icons/baseline_search_black_48dp.png"
 
     toolbar_grp: Group = toga.Group('Toolbar')
 
@@ -23,8 +24,16 @@ def build(app):
         group=toolbar_grp
     )
 
-    app.commands.add(settings_cmd)
-    app.main_window.toolbar.add(settings_cmd)
+    search_cmd = toga.Command(
+        search_action,
+        label='Search',
+        tooltip='Search',
+        icon=search_icon,
+        group=toolbar_grp
+    )
+
+    #app.commands.add(settings_cmd, search_cmd)
+    app.main_window.toolbar.add(search_cmd, settings_cmd)
 
     return main_box
 
@@ -33,7 +42,15 @@ def settings_action(widget):
     print("Settings")
 
 
+def search_action(widget):
+    print("Search")
+
+
 def main():
+    """
+    Function to call main function with title and startup method
+    :return:
+    """
     return toga.App('Supafreak Newsticker', startup=build)
 
 
